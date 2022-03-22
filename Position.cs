ef8954ac417fda999066d342e1900547796dbf86
ArvidSkola <93994217+ArvidSkola@users.noreply.github.com>
2022-03-22 15:49:04 +0100

    Create Position.cs

---
+++ Position.cs
@@ -0,0 +1,17 @@
+using System;
+namespace Tetris
+	//den här klassen hanterar blockens position.//
+{
+	public class Position
+	{
+		public int Row { get; set; }
+		public int Column { get; set; }
+
+		public Position(int row, int column)
+		{
+			Row  =  row;
+			Column = column;
+		}
+	}
+}
+	
