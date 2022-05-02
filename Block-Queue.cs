7fd7428bf75e8f49bc85f1cb5445811285627010
ArvidSkola <93994217+ArvidSkola@users.noreply.github.com>
2022-05-02 16:24:10 +0200

    Update Block-Queue.cs

--- Block-Queue.cs
+++ Block-Queue.cs
@@ -1,17 +1,18 @@
 using System;
+
 namespace Tetris
 {
     public class Block_Queue
     {
         private readonly Block[] blocks = new Block[]
         {
-            new I_Block,
-            new O_Block,
-            new J_Block,
-            new L_Block,
-            new T_Block,
-            new Z_Block,
-            new S_Block
+            new I_Block(),
+            new O_Block(),
+            new J_Block(),
+            new L_Block(),
+            new T_Block(),
+            new Z_Block(),
+            new S_Block()
 
         };
         private readonly Random random = new Random();
@@ -20,12 +21,13 @@ namespace Tetris
         {
             Next_Block = RandomBlock();
         }
-        private Block_Queue RandomBlock()
+        private Block RandomBlock()
         {
             return blocks[random.Next(blocks.Length)];
         }
         public Block Generate_Block()
         {
+            Block block = Next_Block;
             do
             {
                 Next_Block = RandomBlock();
@@ -33,7 +35,7 @@ namespace Tetris
             while (block.Id == Next_Block.Id);
             return block;
         }
-            
+
     }
 
 }
