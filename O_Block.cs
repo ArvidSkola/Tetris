ded9d3ba5d858f15599d634437ee4e1c2088f554
ArvidSkola <93994217+ArvidSkola@users.noreply.github.com>
2022-05-03 15:00:41 +0200

    Update O_Block.cs

--- O_Block.cs
+++ O_Block.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//här hanteras O blockets olika rotationer.//
 namespace Tetris
 {
     public class O_Block:Block 
@@ -12,6 +12,7 @@ namespace Tetris
         {
             new Position[]{new(0,0),new(0,1),new(1,0),new(1,1)}
         };
+        //Eftersom O blockets position inte ändras om man roterar det behöver man inte ha mer än en position i arrayen.//
         public override int Id => 2;
         protected override Position StartOffset => new Position(0, 4);
         protected override Position[][] Tiles => tiles;
