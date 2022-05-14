445ef99b9f0288483245795f4cf57fc53b1dd5e8
ArvidSkola <93994217+ArvidSkola@users.noreply.github.com>
2022-05-14 23:20:49 +0200

    Update I_Combo.cs

--- I_Combo.cs
+++ I_Combo.cs
@@ -14,9 +14,10 @@ namespace Tetris
             new Position[]{new(0,4),new(1,4),new(2,4),new(3,4),new(4,4),new(5,4),new(6,4),new(7,4)},
             new Position[]{new(4,0),new(4,1),new(4,2),new(4,3),new(4,4),new(4,5),new(4,6),new(4,7)},
             new Position[]{new(0,3),new(1,3),new(2,3),new(3,3),new(4,3),new(5,3),new(6,3),new(7,3)}           
-        }
+        };
          protected override Position StartOffset => new Position(-1, 3);
         protected override Position[][] Tiles => tiles;
+        public override int Id => 11;
 
     }
 }
