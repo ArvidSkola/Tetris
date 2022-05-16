24373802c2ecbf9673dda34d39bb5faa1c1f5b14
ArvidSkola <93994217+ArvidSkola@users.noreply.github.com>
2022-05-16 19:28:53 +0200

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,9 +15,7 @@ using System.Windows.Shapes;
 
 namespace Tetris
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+  
     public partial class MainWindow : Window
     {
         private readonly ImageSource[] tileImages = new ImageSource[]
