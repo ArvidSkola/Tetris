f2a6ce0a1cf7ddac8105864ca7abbf2960d57881
ArvidSkola <93994217+ArvidSkola@users.noreply.github.com>
2022-05-23 22:41:48 +0200

    Update Game_State.cs

--- Game_State.cs
+++ Game_State.cs
@@ -1,7 +1,7 @@
 using System;
-namespace Tetris 
-    //Den här klassen kommer kombinera alla funktioner i de andra .cs klasserna för att hantera hur spelet ändras medans man spelar det.//
+namespace Tetris
 {
+    //Den här klassen kommer kombinera alla funktioner i de andra .cs klasserna för att hantera hur spelet ändras medans man spelar det.//
     public class Game_State
     {
         private Block currentBlock;
@@ -14,33 +14,45 @@ namespace Tetris
                 currentBlock.Reset();
                 for (int i = 0; i < 2; i++)
                 {
-                    currentBlock.Move(1, 0);
-                    if (BlockFits())
+                    currentBlock.Move_Block(1, 0);
+                    if (!BlockFits())
                     {
-                        currentBlock.Move(-1, 0);
+                        currentBlock.Move_Block(-1, 0);
                     }
                 }
             }
         }
         public Grid Grid { get; }
         public Block_Queue Block_Queue { get; }
-        public bool Lose { get; private set; }
+        public bool YouLose { get; private set; }
         public int Score { get; private set; }
-        public Block Held_Blocks { get; private set; }
+        public Block? Held_Block { get; private set; }
         public bool CanHold { get; private set; }
-//Här skapas variabler och booler som kommer hantera poängsystemet, förlust, sparade block och om man kan spara ett block eller inte.//
+        public bool CanHold2 { get; private set; }
+        public Block? Held_Block2 { get; private set; }
+        //Här skapas variabler och booler som kommer hantera poängsystemet, förlust, sparade block och om man kan spara ett block eller inte.//  
+
+
+
         public Game_State()
         {
-            Grid = new Grd(22, 10);
-            Block_Queue = new Block_Queue;
+            Grid = new Grid(22, 10);
+            Block_Queue = new Block_Queue();
             CurrentBlock = Block_Queue.Generate_Block();
-             CanHold = true;
+            CanHold = true;
             CanHold2 = true;
+            
+            
+            
+
+            
         }
 
+        //Att veta om ett block får plats är viktigt för programet när spelaren ska placera blocken. Boolen kommer gå i genom alla rutor under blocket för att se om de 
+        // är tomma eller inte. Är minst en full kommer boolen retuneras som falsk annars retuneras den som san.//
         private bool BlockFits()
         {
-            foreach (Position p in CurrentBlock.TilePosition())
+            foreach (Position p in CurrentBlock.TilePosistion())
             {
                 if (!Grid.CellEmpty(p.Row, p.Column))
                 {
@@ -49,24 +61,31 @@ namespace Tetris
             }
             return true;
         }
-        //Att veta om ett block får plats är viktigt för programet när spelaren ska placera blocken. Boolen kommer gå i genom alla rutor under blocket för att se om de 
-        // är tomma eller inte. Är minst en full kommer boolen retuneras som falsk annars retuneras den som san.//
         public void Hold_Block()
         {
-            if (!CanHold)
+            if (!CanHold && CurrentBlock.Id > 7)
             {
                 return;
             }
-            if (Held_Blocks < 2)
+
+            if (Held_Block == null)
             {
-                Held_Blocks = CurrentBlock;
+                Held_Block = CurrentBlock;
                 CurrentBlock = Block_Queue.Generate_Block();
             }
+            else
+            {
+                Block tmp = CurrentBlock;
+                CurrentBlock = Held_Block;
+                Held_Block = tmp;
+            }
+
+            CanHold = false;
 
         }
-         public void Hold_Block2()
+        public void Hold_Block2()
         {
-            if (!CanHold2)
+            if (!CanHold2 && CurrentBlock.Id > 7)
             {
                 return;
             }
@@ -86,10 +105,6 @@ namespace Tetris
             CanHold2 = false;
 
         }
-        //den här funktionen hanterar sparandet av blocken. I början av spelaet skapas en bol, CanHold som är avgörande för 
-        //denna funktionen. Om den är lika med falskt kommer man att retuneras och ens block kommer inte att ändras.
-        //Men om den är san kommer blocket Held_Block ändras till ditt nuvarande block och ett nytt kommer att skapas.
-        
         public void Block_Rotate_CW()
         {
             CurrentBlock.Rotate_CW();
@@ -124,15 +139,14 @@ namespace Tetris
         }
         //Dessa funktioner kommer att kopplas till vissa knappar på ditt tangentbord i en annan fil och kommer att styra ditt block.//
         //När dessa kallas kommer de utföras så länge blocket inte får plats. Om detta är fallet kommer den motsats rörelsen ske. 
-         //Tex om du vill flytta ditt block åt höger men det får 
+        //Tex om du vill flytta ditt block åt höger men det får 
         //inte plats kommer det flyttas åt vänster för att motverka detta.
-
-        public bool Lose()
+        private bool Lose()
         {
             return !(Grid.RowEmpty(0) && Grid.RowEmpty(1));
         }
 
-       private void Place_Block()
+        private void Place_Block()
         {
             foreach (Position p in CurrentBlock.TilePosistion())
             {
@@ -160,7 +174,6 @@ namespace Tetris
 
             }
         }
-
         public void Block_Move_Down()
         {
             CurrentBlock.Move_Block(1, 0);
@@ -172,7 +185,7 @@ namespace Tetris
         }
         //Denna funktionen kommer att flytta ditt block nedåt och kommer både styras med knapptryck samt hända konstant under spelets gång.
         //Precis som i de andra funktionerna som hanterade rörelse kommer den motsatta rörelsen ske om blocket inte får plats dvs det flyttas uppåt.
-        private int TileDropDistance()
+        private int TileDropDistance(Position p)
         {
             int drop = 0;
             while (Grid.CellEmpty(p.Row + drop + 1, p.Column))
@@ -185,19 +198,57 @@ namespace Tetris
         public int BlockDropDistance()
         {
             int drop = Grid.Rows;
-            foreach (Psosition p in CurrentBlock.TilesPostition)
+            foreach (Position p in CurrentBlock.TilePosistion())
             {
-                drop = System.Math.Min(drop, TileDropDistance(p))
+                drop = System.Math.Min(drop, TileDropDistance(p));
+               
             }
 
             return drop;
         }
         public void DropBlock()
         {
-            CurrentBlock.Move(BlockDropDistance, 0)
+            CurrentBlock.Move_Block(BlockDropDistance(), 0);
             Place_Block();
         }
+        public void CombineBlock()
+        {
+           
+            switch(Held_Block)
+            {
+                case I_Block:
+                    Held_Block = CurrentBlock;
+                    CurrentBlock = new I_Combo();
+                    Held_Block2 = null;
+                    
+                    break;
+                case O_Block:
+                    Held_Block = CurrentBlock;
+                    CurrentBlock = new O_Combo();
+                    Held_Block2 = null;
+                    
+                    break;
+                case J_Block:
+                    Held_Block = CurrentBlock;
+                    CurrentBlock = new J_Combo();
+                    Held_Block2 = null;
+                    break;
+                case L_Block:
+                    Held_Block = CurrentBlock;
+                    CurrentBlock = new L_Combo();
+                    Held_Block2 = null;
+                    
+                    break;
+                case T_Block:
+                    Held_Block = CurrentBlock;
+                    CurrentBlock = new T_Combo();
+                    Held_Block2 = null;               
+                    break;
+            }
+        }
+        
+
+        
 
     }
-
 }
